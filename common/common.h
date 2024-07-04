0a5a0e5f408a6d1bca88d5a8e5cba8f8d1aa4f01
Astrodynamic <Astrodynamic.fkg@gmail.com>
2024-07-04 18:06:37 +0300

    start for learning asio

--- common/common.h
+++ common/common.h
@@ -1,14 +1,1 @@
 #pragma once
-
-#include <string>
-
-struct Order {
-  int client_id;
-  double amount;
-  double price;
-  std::string side;
-
-  bool operator<(const Order& other) const {
-    return price < other.price;
-  }
-};
--- server/src/main.cc
+++ server/src/main.cc
@@ -5,8 +5,6 @@
 int main(int argc, char* argv[]) {
   try {
     boost::asio::io_context io_context;
-    Server server(io_context, std::atoi(argv[2]));
-    io_context.run();
   } catch (std::exception& e) {
     std::cerr << "Exception: " << e.what() << "\n";
   }
